f7b69f8dedb3d8bb0af29828c46e62dfd4f8de31
roselandroche <r.landroche@gmail.com>
2020-08-23 13:00:05 -0400

    Working on ancestor - U step

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,27 @@
 
 def earliest_ancestor(ancestors, starting_node):
     pass
+
+'''
+U
+Input
+    List of (parent, child) pairs
+    Each individual is unique
+
+Output
+    Return the individual furthest away from the input individual
+    If more than one at that level of distance:
+        Return the individual with lowest numeric id
+    If input individual has no parents:
+        Return -1
+
+Have to traverse from starting_node to end of all ancestors and return the longest possible path's end
+    (but backwards)
+Suggested to use DFT, but that is random... how to make sure it returns the longest path?
+Could do BFT, would ensure that the last path found would be longest...
+Graph is uni-directional, because it's a parent child relationship
+    Have to search from the top down
+    
+P
+
+'''
